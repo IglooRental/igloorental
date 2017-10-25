94eb6a26773191f27708278d180525f9542d5af5
Sašo Stanovnik <sstanovnik@gmail.com>
2017-10-25 10:45:30 +0200

    Update project structure, add ir-user.

---
+++ subrepo-update.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+git pull --rebase git@github.com:IglooRental/ir-image-retrieval.git
+git pull --rebase git@github.com:IglooRental/ir-image-upload.git
+git pull --rebase git@github.com:IglooRental/ir-messaging.git
+git pull --rebase git@github.com:IglooRental/ir-payment.git
+git pull --rebase git@github.com:IglooRental/ir-property-catalogue.git
+git pull --rebase git@github.com:IglooRental/ir-property-listing.git
+git pull --rebase git@github.com:IglooRental/ir-property-rental.git
+git pull --rebase git@github.com:IglooRental/ir-recommendation.git
+git pull --rebase git@github.com:IglooRental/ir-review.git
+git pull --rebase git@github.com:IglooRental/ir-user.git
+git pull --rebase git@github.com:IglooRental/ir-web.git
