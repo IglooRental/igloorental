fa40969f4678bcc83434e57e34c8c790cd1cf38b
Sašo Stanovnik <sstanovnik@gmail.com>
2017-10-25 10:03:36 +0200

    Removed submodules, added manual init.

---
+++ initialize.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+git clone git@github.com:IglooRental/ir-image-retrieval.git
+git clone git@github.com:IglooRental/ir-image-upload.git
+git clone git@github.com:IglooRental/ir-messaging.git
+git clone git@github.com:IglooRental/ir-payment.git
+git clone git@github.com:IglooRental/ir-property-catalogue.git
+git clone git@github.com:IglooRental/ir-property-listing.git
+git clone git@github.com:IglooRental/ir-property-rental.git
+git clone git@github.com:IglooRental/ir-recommendation.git
+git clone git@github.com:IglooRental/ir-review.git
+git clone git@github.com:IglooRental/ir-user.git
+git clone git@github.com:IglooRental/ir-web.git
