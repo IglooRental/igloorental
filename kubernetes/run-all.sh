56a83731d75e54e5c162cecf76e4eb49817931f8
jm5619 <jm5619@student.uni-lj.si>
2017-11-29 11:26:18 +0100

    Add database container and config.

--- kubernetes/run-all.sh
+++ kubernetes/run-all.sh
@@ -3,6 +3,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 kubectl create -f $DIR/etcd-deployment.yml
 kubectl create -f $DIR/etcd-service.yml
+kubectl create -f $DIR/postgres-deployment.yaml
+kubectl create -f $DIR/postgres-service.yaml
 kubectl create -f $DIR/ir-user-deployment.yml
 kubectl create -f $DIR/ir-user-service.yml
 kubectl create -f $DIR/ir-property-catalogue-deployment.yml
