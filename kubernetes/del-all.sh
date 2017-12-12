ecd8f9c1a2cea29b8fbeae7b427bfe5171efb83a
jm5619 <jm5619@student.uni-lj.si>
2017-12-12 10:02:25 +0100

    minor tweaks

--- kubernetes/del-all.sh
+++ kubernetes/del-all.sh
@@ -2,7 +2,9 @@
 
 kubectl delete svc ir-user
 kubectl delete svc ir-property-catalogue
+kubectl delete svc postgres
 kubectl delete svc etcd
 kubectl delete deploy ir-user
 kubectl delete deploy ir-property-catalogue
+kubectl delete deploy postgres
 kubectl delete deploy etcd
